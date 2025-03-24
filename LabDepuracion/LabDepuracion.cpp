49b500d428ab272ed3c30a2479a7e9146b30c704
alumno <alumno@VN-3I3>
2025-03-24 09:53:28 -0600

    Agregar archivos de proyecto.

---
+++ LabDepuracion/LabDepuracion.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+// Función recursiva para calcular el factorial
+int factorial(int n) {
+	if (n < 0) {
+		throw invalid_argument("Error: Factorial de un número negativo no está definido.");
+	}
+	if (n == 0 || n == 1) {
+		return 1;
+	}
+	return n * factorial(n - 1);
+}
+int main() {
+	try {
+		int numero;
+		cout << "Introduce un número para calcular el factorial: ";
+		cin >> numero;
+		int resultado = factorial(numero);
+		cout << "El factorial de " << numero << " es: " << resultado << endl;
+	}
+	
+	catch (...) {
+		cerr << "Se ha producido un error desconocido." << endl;
+	}
+
+	return 0;
+
+}
+
